f717e00441f58f99aec782aba1a8bd77a624236a
Idris Eslam <idrissislam7@gmail.com>
2024-07-01 15:12:57 +0300

    Add files via upload

---
+++ subs_portal/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "HR Expense Portal",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Human Resources",
+    "summary": "Show current expense in a website.",
+    "author": "Idris Eslam Idris",
+    "website": "mzn.sa",
+    "depends": ["subscription_package", "web"],
+    "data": [
+        "views/customer_template.xml",
+    ],
+    "installable": True,
+}
